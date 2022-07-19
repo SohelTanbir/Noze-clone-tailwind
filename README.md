e8f39a3a7d0c3587936b3d0aa4163770f75122b5
SohelTanbir <soheltanbir19@gmail.com>
2022-07-19 22:22:35 +0600

    NOZE clone started

---
+++ README.md
@@ -0,0 +1,1 @@
+# Noze-clone-tailwind
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'navColor':'#b9b0e4',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
